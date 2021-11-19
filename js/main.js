78bd16d1ce26e5e0231b7999bbabeeb6c8595b42
pierfrancescoscar <scarinci401@gmail.com>
2021-11-19 15:23:08 +0100

    ADD: general setup

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Descrizione:
+// Attraverso l’apposita API di Boolean
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
