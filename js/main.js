e4ae2528fee85bdf875bc0e9dc78946cd3cc0a53
pierfrancescoscar <scarinci401@gmail.com>
2021-11-19 16:03:11 +0100

    ADD: mail generator w/axios and his method

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@
 
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        mailList: [],
+        apiURL: 'https://flynn.boolean.careers/exercises/api/random/mail',
+    },
+    created() {
+        this.mailGenerator();
+    },
+    methods: {
+        mailGenerator() {
+            for(let i = 0; i < 10; i++) {
+                axios.get(this.apiURL).then(response => {
+                    console.log(response.data.response);
+                    response.data.response;
+                    this.mailList.push(response.data.response);
+                })
+            }
+        } 
+    }
+})
